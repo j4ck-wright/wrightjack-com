bd2fe5e0b00e7426140b865a41026d7c59e70e33
Jack Wright <jackw221001@gmail.com>
2024-03-13 22:10:52 +0000

    test: add @ alias in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
     test: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
         environment: 'jsdom',
         globals: true,
     },
