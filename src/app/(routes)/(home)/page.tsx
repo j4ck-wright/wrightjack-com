b913753f21a9009309db6303110848e49f22abeb
Jack Wright <jackw221001@gmail.com>
2024-03-13 22:23:33 +0000

    fix: remove duplicate <main />

--- src/app/(routes)/(home)/page.tsx
+++ src/app/(routes)/(home)/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
     return (
-        <main>
+        <>
             <h1 className="text-4xl">
                 <span>Next.js</span> +{' '}
                 <span className="text-blue-800">Typescript</span> +{' '}
                 <span className="text-blue-600">Tailwind</span>
             </h1>
-        </main>
+        </>
     );
 }
