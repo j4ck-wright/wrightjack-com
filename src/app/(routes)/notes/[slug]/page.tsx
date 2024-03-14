34f72bc09117dd4d28a86ebfa1a9308014463d9c
Jack Wright <jackw221001@gmail.com>
2024-03-14 22:49:14 +0000

    feat: replace MDXRemote with compileMDX

--- src/app/(routes)/notes/[slug]/page.tsx
+++ src/app/(routes)/notes/[slug]/page.tsx
@@ -1,13 +1,19 @@
 import '@/app/article.css';
 
-import { MDXRemote } from 'next-mdx-remote/rsc';
+import { Post } from '@/types/post';
 import { PostHandler } from '@/PostHandler';
+import { compileMDX } from 'next-mdx-remote/rsc';
 
 export default async function page({ params }: { params: { slug: string } }) {
     const post = new PostHandler(params.slug);
+    const { content, frontmatter } = await compileMDX<Partial<Post.Metadata>>({
+        options: { parseFrontmatter: true },
+        source: post.getContent(),
+    });
     return (
         <article>
-            <MDXRemote source={post.getContent()} />
+            <h1>{frontmatter.title}</h1>
+            {content}
         </article>
     );
 }
