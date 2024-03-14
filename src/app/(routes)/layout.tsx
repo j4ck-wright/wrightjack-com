42dacc33f1d5b7a60a5bb2a0c6d7eedcb6097ff7
Jack Wright <jackw221001@gmail.com>
2024-03-14 23:01:51 +0000

    fix: remove deconstructed export

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from '@/components/Footer';
-import { Navbar } from '@/components/Navbar';
+import Navbar from '@/components/Navbar';
 import { UI } from '@/types/ui';
 
 const navbarCtas: UI.Cta[] = [
