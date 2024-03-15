1d7a840f5cd21b396ab89223d196c6715b8e0248
Jack Wright <jackw221001@gmail.com>
2024-03-15 20:14:23 +0000

    feat: mdx rendering with metadata

--- src/app/(routes)/notes/[slug]/page.tsx
+++ src/app/(routes)/notes/[slug]/page.tsx
@@ -1,21 +1,40 @@
 import '@/styles/article.css';
 
-import { Post } from '@/types/post';
-import { compileMDX } from 'next-mdx-remote/rsc';
-import { readFileSync } from 'fs';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
 
-export default async function page({ params }: { params: { slug: string } }) {
-    const postContent = readFileSync(`posts/${params.slug}/body.mdx`, 'utf-8');
+export async function generateStaticParams() {
+    const files = fs.readdirSync(path.join('posts/'));
 
-    const { content, frontmatter } = await compileMDX<Partial<Post.Metadata>>({
-        options: { parseFrontmatter: true },
-        source: postContent,
-    });
+    const paths = files.map((filename) => ({
+        slug: filename.replace('.mdx', ''),
+    }));
+
+    return paths;
+}
+
+function getPost({ slug }: { slug: string }) {
+    const markdownFile = fs.readFileSync(`posts/${slug}/body.mdx`, 'utf-8');
+
+    const { data: frontMatter, content } = matter(markdownFile);
+
+    return {
+        content,
+        frontMatter,
+        slug,
+    };
+}
+
+export default function Post({ params }: any) {
+    const props = getPost(params);
 
     return (
-        <article>
-            <h1>{frontmatter.title}</h1>
-            {content}
+        <article className="prose prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert mx-auto">
+            <h1>{props.frontMatter.title}</h1>
+
+            <MDXRemote source={props.content} />
         </article>
     );
 }
