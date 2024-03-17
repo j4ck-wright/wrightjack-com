e03ea720a36836b32336821edc788afd895aa9e7
Jack Wright <jackw221001@gmail.com>
2024-03-17 12:00:58 +0000

    style: add mobile responsiveness

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,10 +3,10 @@ import { UI } from '@/types/ui';
 export default function Navbar({ ctas }: { ctas: UI.Cta[] }) {
     const validCtas = ctas.filter((cta) => cta.href && cta.text);
     return (
-        <nav className="flex gap-12 pt-6 pb-2 px-6">
+        <nav className="flex justify-center gap-6 pt-4 bg-white z-index-50 md:relative md:justify-normal md:gap-12 md:pt-6 md:pb-2 md:px-6">
             {validCtas.map((cta, index) => {
                 return (
-                    <a className="text-2xl" key={index} href={cta.href}>
+                    <a className="md:text-2xl" key={index} href={cta.href}>
                         {cta.text}
                     </a>
                 );
