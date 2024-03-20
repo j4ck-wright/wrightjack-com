ef128913aba93aee3b778194a0a11fb47476b159
Jack Wright <jackw221001@gmail.com>
2024-03-20 20:54:16 +0000

    test: add date check on post validator

--- src/PostValidator/PostValidator.spec.ts
+++ src/PostValidator/PostValidator.spec.ts
@@ -1,5 +1,6 @@
 import { Post } from '@/types/post';
 import { checkHeaderOrder } from './helpers';
+import { formatDate } from '@/utils/dateFormatter';
 import { getAllPosts } from '@/PostHandler';
 
 const prefix = '[POST-VALIDATION]:';
@@ -24,6 +25,10 @@ describe('Post checking', () => {
             metadataItemExists('author', metadata, slug);
             metadataItemExists('title', metadata, slug);
             metadataItemExists('date', metadata, slug);
+            metadataItemExists('slug', metadata, slug);
+            metadataItemExists('description', metadata, slug);
+            metadataItemExists('minutes_to_read', metadata, slug);
+            metadataItemExists('published', metadata, slug);
         });
     });
 
@@ -32,7 +37,7 @@ describe('Post checking', () => {
             const pattern = /^# \S.+/gm;
             if (pattern.test(content)) {
                 throw new Error(
-                    `${prefix} ${slug}.mdx contains one or more header 1. h1 is reserved for the ArticleHeader\n`
+                    `${prefix} ${slug}.mdx contains one or more header 1. h1 is reserved for the ArticleHeader`
                 );
             }
         });
@@ -45,6 +50,18 @@ describe('Post checking', () => {
             if (!correctOrder) {
                 throw new Error(
                     `${prefix} ${slug}.mdx has incorrect header ordering. Check if you've missed a header or have a higher header nested in a lower one`
+                );
+            }
+        });
+    });
+
+    it('has valid date', () => {
+        allPosts.forEach(({ metadata, slug }) => {
+            const date = formatDate(metadata.date as string);
+
+            if (date === '') {
+                throw new Error(
+                    `${prefix} ${slug}.mdx has incorrect date format YYYY/MM/DD (${metadata.date})`
                 );
             }
         });
